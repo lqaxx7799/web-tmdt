1dd42286d86d43a22e1a7827058b5e2940f1cc0d
anhlq1 <anhlq1@novaon.vn>
2023-04-18 13:20:07 +0700

    data the coffee house

--- thecoffeehouse/assets/js/detail.js
+++ thecoffeehouse/assets/js/detail.js
@@ -16,22 +16,34 @@ document.addEventListener('DOMContentLoaded', () => {
   document.getElementById('productDescription').innerHTML = product.description;
   document.getElementById('productImage').src = product.image;
 
+  const sizesTemplate = product.sizes.reduce((res, val) => {
+    const sizeItemTemplate = `<div class="option__item">${val}</div>`;
+    return res + sizeItemTemplate;
+  }, '');
+  document.getElementById('sizeOptions').innerHTML = sizesTemplate;
+
+  const toppingsTemplate = product.toppings.reduce((res, val) => {
+    const toppingItemTemplate = `<div class="option__item">${val}</div>`;
+    return res + toppingItemTemplate;
+  }, '');
+  document.getElementById('toppingOptions').innerHTML = toppingsTemplate;
+
   const suggestionsTemplate = product.suggestions.reduce((res, val) => {
+    const product = products.find(item => item.id === val);
     const suggestionItemTemplate = `<div class="product-suggestion__item">
-      <a class="" href="detail.html?id=${val.id}">
-        <img alt="" src="${val.image}" class="product-suggestion__image"/>
+      <a class="" href="detail.html?id=${product?.id}">
+        <img alt="" src="${product?.image}" class="product-suggestion__image"/>
       </a>
       <div>
         <div class="product-suggestion__title">
-          <a href="detail.html?id=${val.id}">
-            ${val.title}
+          <a href="detail.html?id=${product?.id}">
+            ${product?.title}
           </a>
         </div>
-        <div class="product-suggestion__price">${val.price}</div>
+        <div class="product-suggestion__price">${product?.price}</div>
       </div>
     </div>`;
     return res + suggestionItemTemplate;
   }, '');
-
   document.getElementById('productSuggestions').innerHTML = suggestionsTemplate;
 });
